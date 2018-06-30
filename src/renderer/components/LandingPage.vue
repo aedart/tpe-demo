1c7d5d8f00732bfaa095b688fbf62854aee1012c
Alin Eugen Deac <aedart@gmail.com>
2018-06-30 15:47:37 +0200

    Cleanup

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -33,13 +33,13 @@
   import SystemInformation from './LandingPage/SystemInformation'
 
   export default {
-    name: 'landing-page',
-    components: { SystemInformation },
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
+      name: 'landing-page',
+      components: { SystemInformation },
+      methods: {
+          open (link) {
+              this.$electron.shell.openExternal(link)
+          }
       }
-    }
   }
 </script>
 
--- src/renderer/components/LandingPage/SystemInformation.vue
+++ src/renderer/components/LandingPage/SystemInformation.vue
@@ -32,16 +32,16 @@
 
 <script>
   export default {
-    data () {
-      return {
-        electron: process.versions['atom-shell'],
-        name: this.$route.name,
-        node: process.versions.node,
-        path: this.$route.path,
-        platform: require('os').platform(),
-        vue: require('vue/package.json').version
+      data () {
+          return {
+              electron: process.versions['atom-shell'],
+              name: this.$route.name,
+              node: process.versions.node,
+              path: this.$route.path,
+              platform: require('os').platform(),
+              vue: require('vue/package.json').version
+          }
       }
-    }
   }
 </script>
 

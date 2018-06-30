70b52c5480ad400ebd715cb7ad5b3b75aae614bf
Alin Eugen Deac <aedart@gmail.com>
2018-06-30 16:24:47 +0200

    Add app title and author

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app" class="container">
     <router-view></router-view>
+
+    <nav class="navbar navbar-light bg-light justify-content-end">
+      <span class="navbar-text font-italic">
+        PERT Three Point Estimation Demo, By Alin Eugen Deac
+      </span>
+    </nav>
   </div>
 </template>
 
